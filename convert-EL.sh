456e20f583c122eebd24b87c7e8cdef999998062
pirat28 <pirat28hh@gmx.de>
2020-07-21 06:04:01 +0200

    update

--- convert-EL.sh
+++ convert-EL.sh
@@ -1,13 +1,13 @@
 ##########################################################################
 #
 # EasyList List generator
-# Version 1.7
+# Version 1.8
 #
+# Version 1.8 does not import IP addresses from the Easylist / Fanboy.
 #
-
 
 echo "\n----------------------------\n- EasyList List Generator\n----------------------------"
-echo "Version 1.7\n"
+echo "Version 1.9\n"
 
 ##########################################################################
 #
@@ -18,7 +18,6 @@ echo "Version 1.7\n"
 #
 
 
-
 echo "Adding EasyList Lists\n"
 
 curl -s -L https://fanboy.co.nz/fanboy-problematic-sites.txt https://fanboy.co.nz/r/fanboy-complete.txt https://fanboy.co.nz/r/fanboy-ultimate.txt https://fanboy.co.nz/enhancedstats.txt https://fanboy.co.nz/fanboy-antifacebook.txt https://fanboy.co.nz/fanboy-antifonts.txt https://secure.fanboy.co.nz/malwaredomains_full.txt https://secure.fanboy.co.nz/easylist.txt https://secure.fanboy.co.nz/easyprivacy.txt https://easylist.to/easylistgermany/easylistgermany.txt https://easylist-downloads.adblockplus.org/easylistitaly.txt https://easylist-downloads.adblockplus.org/easylistdutch.txt https://easylist-downloads.adblockplus.org/liste_fr.txt https://easylist-downloads.adblockplus.org/easylistchina.txt http://stanev.org/abp/adblock_bg.txt https://raw.githubusercontent.com/heradhis/indonesianadblockrules/master/subscriptions/abpindo.txt https://easylist-downloads.adblockplus.org/Liste_AR.txt https://raw.githubusercontent.com/tomasko126/easylistczechandslovak/master/filters.txt https://notabug.org/latvian-list/adblock-latvian/raw/master/lists/latvian-list.txt https://raw.githubusercontent.com/easylist/EasyListHebrew/master/EasyListHebrew.txt http://margevicius.lt/easylistlithuania.txt https://easylist-downloads.adblockplus.org/antiadblockfilters.txt https://easylist-downloads.adblockplus.org/easyprivacy.tpl https://easylist-downloads.adblockplus.org/easyprivacy_nointernational.txt https://easylist-downloads.adblockplus.org/easylist_noelemhide.txt https://easylist-downloads.adblockplus.org/easylist_noadult.txt > adblock.unsorted
@@ -27,7 +26,7 @@ curl -s -L https://fanboy.co.nz/fanboy-problematic-sites.txt https://fanboy.co.n
 
 echo "- Editing list... "
 
-sort -u adblock.unsorted | grep ^\|\|.*\^$ | grep -v \/ > adblock.sorted
+sort -u adblock.unsorted | grep ^\|\|.*\^$ | grep -v \/ | grep -v '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' > adblock.sorted
 
 # Remove extra chars and save list
 
